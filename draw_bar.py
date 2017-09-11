1f8169185e48ef57a7b250387a1380ed86d9d670
Ziang Xiao <ziangxiao5135@gmail.com>
2017-09-11 13:38:31 -0500

    Abstract Draft

--- draw_bar.py
+++ draw_bar.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
-''' 
+import pymc3
+'''
 objects = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
 objects2 = ('-5', '-4', '-3', '-2','-1', '0', '1', '2', '3', '4', '5')
 y_pos = np.arange(len(objects2))
 performance2 = [0,1,7,11,12,23,16,17,9,4,4]
 performance3 = [0,1,4,8,4,15,14,16,9,3,4]
 performance = [0,0,3,3,8,8,2,1,0,1,0]
-performance4 = [1,2,8,18,17,9,19,20,17,8,11] 
+performance4 = [1,2,8,18,17,9,19,20,17,8,11]
 plt.bar(y_pos, performance4, align='center', alpha=1)
 plt.xticks(y_pos, objects2)
 plt.ylabel('Freq')
@@ -26,5 +27,5 @@ with pm.Model():
     trace = pm.sample(15000, step)
 
 sns.distplot(trace[-5000:]['mu'], label='PyMC3');
-plt.legend() 
+plt.legend()
 plt.show()
